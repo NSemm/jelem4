bc580f92c29de0d5fb3f4c61fb82dd163a102024
NickSemerenko <nickolay.sem@gmail.com>
2021-01-11 23:57:48 +0200

    rework Contats to List

--- src/main/java/com/k7/contacts/ContactsList.java
+++ src/main/java/com/k7/contacts/ContactsList.java
@@ -1,38 +1,39 @@
 package com.k7.contacts;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ContactsList {
-    private Contact[] contacts = new Contact[0];
+    private List<Contact> contacts = new ArrayList<>();
 
-    private void checkIndex(int index) {
-        if (index >= contacts.length || index < 0)
-            throw new IndexOutOfBoundsException();
-    }
+//    private void checkIndex(int index) {
+//        if (index >= contacts.size() || index < 0)
+//            throw new IndexOutOfBoundsException();
+//    }
 
     public Contact get(int index) {
-        checkIndex(index);
-        return contacts[index];
+        return contacts.get(index);
     }
 
     public void set(int index, Contact c) {
-        checkIndex(index);
-        contacts[index] = c;
+        //checkIndex(index);
+        contacts.set(index, c);
     }
 
     public void add(Contact c) {
-        contacts = Arrays.copyOf(contacts, contacts.length + 1);
-        contacts[contacts.length - 1] = c;
+        contacts.add(c);
     }
 
     public void remove(int index) {
-        checkIndex(index);
-        Contact[] temp = Arrays.copyOf(contacts, contacts.length - 1);
-        for (int i = 0; i < temp.length; i++) temp[i] = i < index ? contacts[i] : contacts[i + 1];
-        contacts = temp;
+//        checkIndex(index);
+//        Contact[] temp = Arrays.copyOf(contacts, contacts.length - 1);
+//        for (int i = 0; i < temp.length; i++) temp[i] = i < index ? contacts[i] : contacts[i + 1];
+//        contacts = temp;
+        contacts.remove(index);
     }
 
     public int size() {
-        return contacts.length;
+        return contacts.size();
     }
 }
